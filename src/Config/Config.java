9aed2d8d825e0320703b5c14805325d607c59243
TitamSeptian <dwiputra4456@protonmail.com>
2022-06-01 01:43:08 +0700

    init project crud daya

---
+++ src/Config/Config.java
@@ -0,0 +1,21 @@
+package Config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+ 
+public class Config {
+    private static Connection mysqlconfig;
+    public static Connection configDB()throws SQLException{
+        try {
+            String url="jdbc:mysql://localhost:4000/pbo_test"; //url database
+            String user="root"; //user database
+            String pass="123qwe123"; //password database
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            mysqlconfig=DriverManager.getConnection(url, user, pass);            
+        } catch (SQLException e) {
+            System.err.println("koneksi gagal "+e.getMessage()); //perintah menampilkan error pada koneksi
+        }
+        return mysqlconfig;
+    }    
+}
